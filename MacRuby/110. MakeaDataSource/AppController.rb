62f7f06a3aa6e8673c80a43209e8287b803509c2
Bodaniel Jeanes <me@bjeanes.com>
2009-01-18 18:38:56 +1000

    AppController created and set up in Nib as datasource for table view and delegate methods implemented

---
+++ MacRuby/110. MakeaDataSource/AppController.rb
@@ -0,0 +1,31 @@
+#
+#  AppController.rb
+#  MakeaDataSource
+#
+#  Created by Bodaniel Jeanes on 18/01/09.
+#  Copyright (c) 2009 Mocra. All rights reserved.
+#
+
+class AppController
+  attr_accessor :items, :newItemField, :toDoTableView
+  
+  def awakeFromNib
+    @items = []
+  end
+  
+  def createNewItem(sender)
+    @items << @newItemField.stringValue
+    @newItemField.stringValue = ''
+    @toDoTableView.reloadData
+  end
+  
+  # Datasource methods:
+  def numberOfRowsInTableView(table)
+    @items.count rescue 0
+  end
+  
+  def tableView(tableView, objectValueForTableColumn: column, row: row)
+    @items[row]
+  end 
+  
+end
